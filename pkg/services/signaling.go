cbcf9dd905b8a80d7aa31a3a5f6d90bcf49756f8
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-05-16 18:41:01 +0200

    feat: Start parser implementation

--- pkg/services/signaling.go
+++ pkg/services/signaling.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	api "github.com/pojntfx/weron/pkg/api/websockets/v1"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
 )
@@ -12,21 +15,64 @@ func Signaling(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		c, err := websocket.Accept(rw, r, nil)
 		if err != nil {
-			log.Println("could not accept on WebSocket: ", err)
+			log.Println("could not accept on WebSocket:", err)
 
 			return
 		}
-		defer c.Close(websocket.StatusInternalError, "internal error")
 
+		msg := "internal error"
+		defer func() {
+			log.Println("could not continue in handler:", msg)
+
+			c.Close(websocket.StatusInternalError, msg)
+		}()
+
+	l:
 		for {
 			var v interface{}
 			if err := wsjson.Read(r.Context(), c, &v); err != nil {
-				log.Println("could not read JSON from WebSocket: ", err)
+				msg = "could not read JSON from WebSocket: " + err.Error()
 
 				break
 			}
 
-			log.Println("received: ", v)
+			vv, ok := v.(map[string]interface{})
+			if !ok {
+				msg = "could not read cast message from WebSocket"
+
+				break
+			}
+
+			t, ok := vv["type"]
+			if !ok {
+				msg = "could not read message type from WebSocket"
+
+				break
+			}
+
+			tt, err := strconv.Atoi(fmt.Sprintf("%v", t))
+			if err != nil {
+				msg = "could not cast message type from WebSocket: " + err.Error()
+
+				break
+			}
+
+			switch tt {
+			case api.TypeApplication:
+				log.Println("received application: ", v)
+			case api.TypeAcceptance:
+				log.Println("received acceptance: ", v)
+			case api.TypeRejection:
+				log.Println("received rejection: ", v)
+			case api.TypeReady:
+				log.Println("received ready: ", v)
+			case api.TypeIntroduction:
+				log.Println("received introduction: ", v)
+			default:
+				msg = fmt.Sprintf("could not handle message type, received unknown message type \"%v\"", tt)
+
+				break l
+			}
 		}
 
 		next.ServeHTTP(rw, r)
