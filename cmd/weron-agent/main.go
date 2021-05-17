f7eb321a22ebe521a032b3d79f79541bdb5ed7aa
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-05-17 19:28:46 +0200

    feat: Add handlers for admission process in agent

--- cmd/weron-agent/main.go
+++ cmd/weron-agent/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"log"
+	"net"
 	"sync"
 
 	api "github.com/pojntfx/weron/pkg/api/websockets/v1"
@@ -12,12 +14,20 @@ import (
 )
 
 func main() {
+	// Define flags
 	raddr := flag.String("raddr", "ws://localhost:15325", "Signaler address")
 	community := flag.String("community", "cluster1", "Community to join")
-	mac := flag.String("mac", "cc:0b:cf:23:22:0d", "MAC address to use")
+	macFlag := flag.String("mac", "cc:0b:cf:23:22:0d", "MAC address to use")
 
+	// Parse flags
 	flag.Parse()
 
+	mac, err := net.ParseMAC(*macFlag)
+	if err != nil {
+		log.Fatal("could not parse MAC address:", err)
+	}
+
+	// Connect to the signaler
 	c, _, err := websocket.Dial(context.Background(), *raddr, nil)
 	if err != nil {
 		log.Fatal("could not dial WebSocket:", err)
@@ -27,10 +37,13 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
+	ready := make(chan struct{})
+
 	go func() {
 		defer wg.Done()
 
 		for {
+			// Read message from connection
 			_, data, err := c.Read(context.Background())
 			if err != nil {
 				log.Println("could not read from WebSocket:", err)
@@ -38,12 +51,55 @@ func main() {
 				return
 			}
 
-			log.Println(string(data))
+			// Parse message
+			var v api.Message
+			if err := json.Unmarshal(data, &v); err != nil {
+				log.Println("could not parse JSON from WebSocket: ", err)
+
+				return
+			}
+
+			// Handle different message types
+			switch v.Type {
+			// Admission
+			case api.TypeRejection:
+				log.Println("handling rejection:", v)
+
+				log.Fatal("could not join community: MAC address rejected. Please retry with another MAC address.")
+			case api.TypeAcceptance:
+				log.Println("handling acceptance:", v)
+
+				ready <- struct{}{}
+			case api.TypeIntroduction:
+				// Cast to introduction
+				var introduction api.Introduction
+				if err := json.Unmarshal(data, &introduction); err != nil {
+					log.Println("could not parse JSON from WebSocket:", err)
+
+					return
+				}
+
+				log.Println("handling introduction:", introduction)
+			}
 		}
 	}()
 
-	if err := wsjson.Write(context.Background(), c, api.NewApplication(*community, *mac)); err != nil {
+	// Send application
+	application := api.NewApplication(*community, mac.String())
+	log.Println("sending application:", application)
+
+	if err := wsjson.Write(context.Background(), c, api.NewApplication(*community, mac.String())); err != nil {
 		log.Fatal("could not send application:", err)
+	}
+
+	<-ready
+
+	// Send ready
+	readyMessage := api.NewReady()
+	log.Println("sending ready:", readyMessage)
+
+	if err := wsjson.Write(context.Background(), c, readyMessage); err != nil {
+		log.Fatal("could not send ready:", err)
 	}
 
 	wg.Wait()
