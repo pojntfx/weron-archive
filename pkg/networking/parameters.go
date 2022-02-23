0b442ac1d84f3fd550d5742d7e2b6c5772c6de42
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-02-23 18:15:18 +0100

    feat: Add support for more target platforms, simplify MAC address allocation

---
+++ pkg/networking/parameters.go
@@ -0,0 +1,11 @@
+//go:build !(windows || linux || darwin)
+
+package networking
+
+import (
+	"github.com/songgao/water"
+)
+
+func AddPlatformParameters(config water.Config, name string) water.Config {
+	return config
+}
---
+++ pkg/networking/parameters_windows.go
@@ -0,0 +1,13 @@
+//go:build windows
+
+package networking
+
+import (
+	"github.com/songgao/water"
+)
+
+func AddPlatformParameters(config water.Config, name string) water.Config {
+	config.PlatformSpecificParams.InterfaceName = name
+
+	return config
+}
