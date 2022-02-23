e7821e0afecad52a7a06f404c48e57c08b7cc92f
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-02-23 19:04:44 +0100

    fix: Set MAC address using Netlink on supported platforms

---
+++ pkg/networking/netlink_unix.go
@@ -0,0 +1,14 @@
+//go:build linux || darwin
+
+package networking
+
+import "github.com/vishvananda/netlink"
+
+func RefreshMACAddress(name string) error {
+	link, err := netlink.LinkByName(name)
+	if err != nil {
+		return err
+	}
+
+	return netlink.LinkSetHardwareAddr(link, link.Attrs().HardwareAddr)
+}
